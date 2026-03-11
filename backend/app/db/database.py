9f060defa961b7212de67a37ab56b154576c892f
subin0910 <subin0910@gachon.ac.kr>
2026-03-12 00:54:56 +0900

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,23 +1,17 @@
 # app/db/database.py
-import os
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-# Base import 유지
+from app.config import settings
 from app.db.model import Base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://aiary_user:aiary_pass@127.0.0.1:5432/aiary_db",
-)
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
 )
 
-# commit 후 객체 재사용 편의를 위해 expire_on_commit=False 설정
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
