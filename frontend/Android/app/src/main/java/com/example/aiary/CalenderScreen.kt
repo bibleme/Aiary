472336aa54d497f5bb6e136bd71e3d77e44f274e
hb913 <112764080+hb913@users.noreply.github.com>
2026-05-22 19:21:42 +0900

    Update CalenderScreen.kt

--- frontend/Android/app/src/main/java/com/example/aiary/CalenderScreen.kt
+++ frontend/Android/app/src/main/java/com/example/aiary/CalenderScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.Delete // 👇 [추가] 휴지통 아이콘
+import androidx.compose.material.icons.filled.Delete 
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
